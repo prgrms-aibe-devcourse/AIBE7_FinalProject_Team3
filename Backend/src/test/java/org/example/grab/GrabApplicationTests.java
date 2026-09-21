e725e12df262b245018702f6e9482bfdf7d7b633
ImJhoon <jhjh9907@gmail.com>
2026-09-21 11:20:42 +0900

    chore: initialize backend project

---
+++ Backend/src/test/java/org/example/grab/GrabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
