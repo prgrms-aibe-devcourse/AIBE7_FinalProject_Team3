8fe6321b8008b169609ac27a2d4c9a5b948043db
ImJhoon <jhjh9907@gmail.com>
2026-09-23 11:55:22 +0900

    chore: UUIDEntity 클래스 추후 네이밍 변경 예정 주석으로 작성

--- Backend/src/main/java/org/example/grab/global/entity/UUIDEntity.java
+++ Backend/src/main/java/org/example/grab/global/entity/UUIDEntity.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 @Getter
 @MappedSuperclass
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+// UUIDEntity 클래스명 보다 의미론적으로 바꿀 수도,,?
 public abstract class UUIDEntity extends BaseEntity {
 
     @Column(name = "public_id", nullable = false, updatable = false, unique = true)
